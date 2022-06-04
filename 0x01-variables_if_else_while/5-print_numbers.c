6b2b8bace2784f800f3e801b1aa4951d06488d01
emmade86 <emmade86@gmail.com>
2022-06-04 04:45:22 +0100

    Newly Added C Library

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 /**
  * main - Entry point
@@ -10,10 +11,9 @@ int main(void)
 {
 	char i = '0';
 
-	while (i <= 9)
+	for (i = '0'; i <= '9'; i++)
 	{
 		putchar(i);
-		i++;
 	}
 	putchar('\n');
 	return (0);
