12ea1e605e6b1cf7577ea33b03f617221e3b5520
emmade86 <emmade86@gmail.com>
2022-06-07 23:57:39 +0100

    Putchar Edit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,22 +1,25 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
- * main - Print "_putchar"
+ * main - Entry point
  *
  * Return: Always 0 (Success)
  */
-int _putchar(char c);
+
 int main(void)
 {
-	int i = 0;
-	char c[] = "_putchar";
-
-	while (i < 8)
-	{
-		_putchar(c[i]);
-		i++;
-	}
-	_putchar('\n');
+	_putchar("_");
+	_putchar("p");
+	_putchar("u");
+	_putchar("t");
+	_putchar("c");
+	_putchar("h");
+	_putchar("a");
+	_putchar("r");
+	_putchar("\n");
 	return (0);
+}
+int _putchar(char c)
+{
+	return(write(1, &c, 1));
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,9 +1,1 @@
-/**
- * header that should add _putchar to library functions
- */
-#include <unistd.h>
-
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
+int _putchar(char c);
