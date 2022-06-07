050358d6ce77cdc267a26d667f4eca199095d4e1
emmade86 <emmade86@gmail.com>
2022-06-07 20:23:30 +0100

    Bugs fixed

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,14 +8,8 @@
 
 int main(void)
 {
-	int i = 0;
 	char p[8] = "_putchar";
 
-	while (i < 8)
-	{
-		_putchar(p[i]);
-		i++;
-	}
-	_putchar('\n');
+	_putchar(char p);
 	return (0);
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,4 +2,9 @@
  * header that should add _putchar to library
  */
 
-void _putchar(char p);
+void _putchar(char p)
+{
+	printf("%c");
+	return (0);
+printf("\n");
+}
