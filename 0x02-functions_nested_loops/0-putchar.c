aaa4e8f4c89eb4c166e2edfa862314de396151c8
emmade86 <emmade86@gmail.com>
2022-06-07 02:06:47 -0700

    Added Putchar task

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char[] put = '_putchar';
+
+	putchar(put);
+	putchar("\n");
+	return (0);
+}
