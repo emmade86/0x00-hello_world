8d1c05af76dad38902ecdf904647c9d2ccacb192
emmade86 <emmade86@gmail.com>
2022-06-09 23:54:04 +0100

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "maiin.h"
+
+/**
+ * mul - multiplies two integers
+ *@a: multiplying
+ *@b: multiplier 
+ * 
+ * Return: res (result) 
+ */
+int mul(int a , int b)
+{
+	int res; 
+
+	res = a * b;
+	return (res);
+}
