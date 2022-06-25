d94a65f51045dbfc1556bec77f4b80de0c9408cd
emmade86 <emmade86@gmail.com>
2022-06-25 02:41:55 +0100

    to be dleted

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,18 +1,33 @@
 #include "main.h"
 
 /**
+<<<<<<< HEAD
  * print_alphabet - Entry point
  * prints the alphabet, followed by a new line.
  * Return: Always 0 (Success)
  */
+=======
+ *  * print_alphabet - Entry point
+ *   * prints the alphabet, followed by a new line.
+ *    * Return: Always 0 (Success)
+  */
+>>>>>>> 2d58a05a555018b9e5f5bec44b765fbb47e897b7
 
 void print_alphabet(void)
 {
 	char c;
 
+<<<<<<< HEAD
 	for (c = 'a'; c <= 'z'; c++)
 	{
 		_putchar(c);
 	}
 	_putchar('\n');
+=======
+			for (c = 'a'; c <= 'z'; c++)
+				{
+						_putchar(c);
+							}
+				_putchar('\n');
+>>>>>>> 2d58a05a555018b9e5f5bec44b765fbb47e897b7
 }
--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,6 +1,7 @@
 #include "main.h"
 
 /**
+<<<<<<< HEAD
  * print_alphabet_x10 - prints alphabet 10 times
  * Return:void
  */
@@ -21,4 +22,26 @@ void print_alphabet_x10(void)
 		_putchar('\n');
 		i++;
 	}
+=======
+ *  * print_alphabet_x10 - prints alphabet 10 times
+ *   * Return:void
+ *    */
+
+void print_alphabet_x10(void)
+{
+		char c;
+			int i = 0;
+
+				while (i < 10)
+						{
+									c = 'a';
+											while (c <= 'z')
+														{
+																		_putchar(c);
+																					c++;
+																							}
+													_putchar('\n');
+															i++;
+																}
+>>>>>>> 2d58a05a555018b9e5f5bec44b765fbb47e897b7
 }
